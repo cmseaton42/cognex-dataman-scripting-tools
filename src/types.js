317528f1a027f6836e867488fa262c77a50a5c53
Canaan Seaton <cmseaton42@gmail.com>
2020-01-11 17:02:08 -0800

    Remove unneeded compiled files

--- src/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EnumDecodedEventsSystem;
-(function (EnumDecodedEventsSystem) {
-    EnumDecodedEventsSystem[EnumDecodedEventsSystem["NONE"] = 0] = "NONE";
-    EnumDecodedEventsSystem[EnumDecodedEventsSystem["GOOD_READ"] = 1] = "GOOD_READ";
-    EnumDecodedEventsSystem[EnumDecodedEventsSystem["NO_READ"] = 2] = "NO_READ";
-    EnumDecodedEventsSystem[EnumDecodedEventsSystem["VALIDATION_FAILURE"] = 3] = "VALIDATION_FAILURE";
-})(EnumDecodedEventsSystem = exports.EnumDecodedEventsSystem || (exports.EnumDecodedEventsSystem = {}));
